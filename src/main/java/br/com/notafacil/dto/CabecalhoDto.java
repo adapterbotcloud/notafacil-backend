59bc597a248401495bf62ea2107f5e3ec5461bbe
CabraBot <cabrabot@users.noreply.github.com>
2026-03-06 16:26:23 +0000

    fix: CabecalhoDto.toString() retorna XML do cabeçalho GINFES v3 (antes retornava toString do record)

--- src/main/java/br/com/notafacil/dto/CabecalhoDto.java
+++ src/main/java/br/com/notafacil/dto/CabecalhoDto.java
@@ -3,4 +3,12 @@ package br.com.notafacil.dto;
 public record CabecalhoDto(
         String versao,
         String versaoDados
-) {}
+) {
+    @Override
+    public String toString() {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+               "<ns2:cabecalho versao=\"" + versao + "\" xmlns:ns2=\"http://www.ginfes.com.br/cabecalho_v03.xsd\">" +
+               "<versaoDados>" + versaoDados + "</versaoDados>" +
+               "</ns2:cabecalho>";
+    }
+}
