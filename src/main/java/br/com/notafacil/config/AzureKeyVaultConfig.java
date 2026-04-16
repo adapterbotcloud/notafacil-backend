06ea871f19cd47deb26d5f72951a528916503490
CabraMestre <cabra@adapterbot.cloud>
2026-04-16 01:32:45 +0000

    fix: use ClientSecretCredentialBuilder instead of direct constructor

--- src/main/java/br/com/notafacil/config/AzureKeyVaultConfig.java
+++ src/main/java/br/com/notafacil/config/AzureKeyVaultConfig.java
@@ -1,6 +1,6 @@
 package br.com.notafacil.config;
 
-import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.security.keyvault.certificates.CertificateClient;
 import com.azure.security.keyvault.certificates.CertificateClientBuilder;
 import com.azure.security.keyvault.jca.KeyVaultJcaProvider;
@@ -35,15 +35,14 @@ public class AzureKeyVaultConfig {
 
 
     @Bean
-    public ClientSecretCredential azureCredential() {
-        return new ClientSecretCredential(tenantId, clientId, clientSecret, null);
-    }
-
-    @Bean
-    public CertificateClient certificateClient(ClientSecretCredential azureCredential) {
+    public CertificateClient certificateClient() {
         return new CertificateClientBuilder()
                 .vaultUrl(vaultUrl)
-                .credential(azureCredential)
+                .credential(new ClientSecretCredentialBuilder()
+                        .tenantId(tenantId)
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .build())
                 .buildClient();
     }
 
